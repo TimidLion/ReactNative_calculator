c88eba115aa618ef596ae259a7908a9ea7ea584e
TimidLion <sgm02186@naver.com>
2017-05-04 03:52:15 +0900

    board added

--- index.android.js
+++ index.android.js
@@ -14,46 +14,83 @@ import {
   View
 } from 'react-native';
 
-class Numbers extends Component {
+class Number extends Component {
   constructor(props){
   	super(props);
   	this.onButtonPress = this.onButtonPress.bind(this);
-  	this.state ={
-  		number: 0
-  	};
   }
 
   onButtonPress(){
-  	Alert.alert(this.props.value.toString());
+  	Alert.alert(this.props.number.toString());
   };
 
   render(){
   	return (
-  		<Button
-		  onPress={this.onButtonPress}
-		  title={this.state.number.toString()}
-		  color="#841584"
-		  accessibilityLabel="Learn more about this purple button"
-		/>
+  		<View style={{width: 50, height: 50}}>
+	  		<Button
+			  onPress={this.onButtonPress}
+			  title={this.props.number.toString()}
+			  color="#841584"
+			  accessibilityLabel="Learn more about this purple button"
+			/>
+		</View>
   	);
   }
 }
 
+class Board extends React.Component {
+  constructor() {
+    super();
+  }
+
+  renderNumber(i) {
+    return <Number number={i} />;
+  }
+
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+      }}>
+        <View style={{flex: 0, flexDirection: 'row'}}>
+          {this.renderNumber(1)}
+          {this.renderNumber(2)}
+          {this.renderNumber(3)}
+          {this.renderNumber(0)}
+        </View>
+        <View style={{flex: 0, flexDirection: 'row'}}>
+          {this.renderNumber(4)}
+          {this.renderNumber(5)}
+          {this.renderNumber(6)}
+        </View>
+        <View style={{flex: 0, flexDirection: 'row', borderWidth: 0}}>
+          {this.renderNumber(7)}
+          {this.renderNumber(8)}
+          {this.renderNumber(9)}
+        </View>
+      </View>
+    );
+  }
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 export default class ReactNative_Calc extends Component {
   constructor(props) {
   	super(props);
   	this.state={
-  		number: 0
+  		value: 0
   	};
   }
 
   render() {
     return (
       <View style={styles.container}>
-      	<Numbers value={this.state.number}/>
+      	<Board value={this.state.value}/>
+      	<View style={{height:100}} />
 		<Text>
-			Nice to meet you
-			abc
+			TimidLion's Calculator
 		</Text>
       </View>
     );
